baec1b0b4e1a5cb73c8105576eead2c38402b4fd
“felipelm” <felipelm123@gmail.com>
2019-02-26 17:11:30 -0300

    add expiration date

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import jwt
-encoded = jwt.encode({'some': 'payload'}, 'secret', algorithm='HS256')
-
+from datetime import datetime
+import time
+# encoded = jwt.encode({'some': 'payload'}, 'secret', algorithm='HS256')
+encoded=jwt.encode({'exp': datetime.utcnow()+datetime.timedelta(seconds=30)}, 'secret')
 print(encoded)
+time.sleep(60)
 decoded = jwt.decode(encoded, 'secret', algorithms=['HS256'])
 print(decoded)
